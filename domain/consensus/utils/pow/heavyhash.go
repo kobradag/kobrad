feb2a97703962098c513dfec6d455a8414baad75
kobradag <168386458+kobradag@users.noreply.github.com>
2024-09-25 17:30:24 -0500

    Update heavyhash.go

--- domain/consensus/utils/pow/heavyhash.go
+++ domain/consensus/utils/pow/heavyhash.go
@@ -77,7 +77,7 @@ func (mat *matrix) HeavyHash(hash *externalapi.DomainHash) *externalapi.DomainHa
 		for j := 0; j < 64; j++ {
 			sum += mat[i][j] * vector[j]
 		}
-                product[i] = (sum & 0xF) ^ ((sum >> 4) & 0xF) ^ ((sum >> 8) & 0xF)
+		product[i] = sum >> 10
 	}
 
 	// Concatenate 4 LSBs back to 8 bit xor with sum1
