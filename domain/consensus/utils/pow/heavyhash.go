4485968dff92500d5123d9588536f79ed662cd61
kobradag <168386458+kobradag@users.noreply.github.com>
2024-10-01 07:58:18 -0500

    Update heavyhash.go

--- domain/consensus/utils/pow/heavyhash.go
+++ domain/consensus/utils/pow/heavyhash.go
@@ -12,8 +12,8 @@ const eps float64 = 1e-9
 type matrix [64][64]uint16
 
 // Define the DAA Score threshold for switching to the new method
-// The value of 10,750,000 marks the transition point to the new HeavyHash method.
-const transitionDAAScore uint64 = 10_750_000
+// The value of 10,860,000 marks the transition point to the new HeavyHash method.
+const transitionDAAScore uint64 = 100
 
 // Function that chooses the appropriate hashing algorithm based on DAA Score
 // If the DAA Score is above the transition threshold, it uses the new HeavyHash method.
@@ -61,33 +61,57 @@ func (mat *matrix) oldHeavyHash(hash *externalapi.DomainHash) *externalapi.Domai
 // New method (your updated logic)
 // This function implements the new version of HeavyHash, which is used after the DAA Score threshold is reached.
 func (mat *matrix) newHeavyHash(hash *externalapi.DomainHash) *externalapi.DomainHash {
-	hashBytes := hash.ByteArray()
-	var vector [64]uint16
-	var product [64]uint16
-	for i := 0; i < 32; i++ {
-		vector[2*i] = uint16(hashBytes[i] >> 4)
-		vector[2*i+1] = uint16(hashBytes[i] & 0x0F)
-	}
-	// Perform matrix-vector multiplication and convert to 4 bits
-	for i := 0; i < 64; i++ {
-		var sum uint16
-		for j := 0; j < 64; j++ {
-			sum += mat[i][j] * vector[j]
-		}
-		// New logic for the sum reduction
-		product[i] = (sum & 0xF) ^ ((sum >> 4) & 0xF) ^ ((sum >> 8) & 0xF)
-	}
+    hashBytes := hash.ByteArray()
 
-	// Concatenate the 4 least significant bits back to 8-bit XOR with sum1
-	var res [32]byte
-	for i := range res {
-		res[i] = hashBytes[i] ^ (byte(product[2*i]<<4) | byte(product[2*i+1]))
-	}
-	// Hash again with new logic
-	writer := hashes.HeavyHashWriter()
-	writer.InfallibleWrite(res[:])
-	return writer.Finalize()
+    var vector [64]uint16
+    var product [64]uint16
+
+    for i := 0; i < 32; i++ {
+        vector[2*i] = uint16(hashBytes[i] >> 4)
+        vector[2*i+1] = uint16(hashBytes[i] & 0x0F)
+    }
+
+    for i := 0; i < 64; i++ {
+        var sum uint16
+        for j := 0; j < 64; j++ {
+            dynamicShift := (vector[j] % 7) + 1  // Ð¡Ð´Ð²Ð¸Ð³ Ð¾Ñ‚ 1 Ð´Ð¾ 7 Ð±Ð¸Ñ‚
+            condition := (vector[j] + mat[i][j]) % 2 == 0
+
+            if condition {
+                sum += (mat[i][j] * vector[j]) << dynamicShift
+            } else {
+                sum += (mat[i][j] * vector[j]) >> dynamicShift
+            }
+        }
+        product[i] = uint16(math.Sin(float64(sum)) * 0xFFFF) ^
+                     uint16(math.Tan(float64(sum)) * 0xFFFF) ^
+                     ((sum >> 4) & 0xF) ^
+                     ((sum >> 8) & 0xF)
+    }
+
+
+    var res [32]byte
+    for i := range res {
+        dynamicShift := (product[2*i] % 5) + 3  
+        res[i] = hashBytes[i] ^ (byte(product[2*i]<<dynamicShift) | byte(product[2*i+1]>>dynamicShift))
+    }
+
+    rounds := (uint32(hashBytes[0]) % 5) + 4  
+    for r := 0; r < int(rounds); r++ {
+        writer := hashes.HeavyHashWriter()
+        writer.InfallibleWrite(res[:])
+        finalizedHash := writer.Finalize()
+
+        res = *finalizedHash.ByteArray()
+    }
+
+    writer := hashes.HeavyHashWriter()
+    writer.InfallibleWrite(res[:])
+    return writer.Finalize()
 }
+
+
+
 
 // Function to generate the matrix (no changes here)
 // This function generates the matrix required for the HeavyHash computation.
@@ -143,4 +167,58 @@ func (mat *matrix) computeRank() int {
 		}
 	}
 	return rank
+}
+
+func (mat *matrix) HeavyHash(hash *externalapi.DomainHash) *externalapi.DomainHash {
+
+	hashBytes := hash.ByteArray()
+
+	var vector [64]uint16
+
+	var product [64]uint16
+
+	for i := 0; i < 32; i++ {
+
+		vector[2*i] = uint16(hashBytes[i] >> 4)
+
+		vector[2*i+1] = uint16(hashBytes[i] & 0x0F)
+
+	}
+
+	// Matrix-vector multiplication, and convert to 4 bits.
+
+	for i := 0; i < 64; i++ {
+
+		var sum uint16
+
+		for j := 0; j < 64; j++ {
+
+			sum += mat[i][j] * vector[j]
+
+		}
+
+		product[i] = (sum & 0xF) ^ ((sum >> 4) & 0xF) ^ ((sum >> 8) & 0xF)
+
+	}
+
+
+
+	// Concatenate 4 LSBs back to 8 bit xor with sum1
+
+	var res [32]byte
+
+	for i := range res {
+
+		res[i] = hashBytes[i] ^ (byte(product[2*i]<<4) | byte(product[2*i+1]))
+
+	}
+
+	// Hash again
+
+	writer := hashes.HeavyHashWriter()
+
+	writer.InfallibleWrite(res[:])
+
+	return writer.Finalize()
+
 }
