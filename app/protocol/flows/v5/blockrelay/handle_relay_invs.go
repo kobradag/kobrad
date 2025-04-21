f68b1d0a669513abafcc89ee3b1049555674227b
kobradag <168386458+kobradag@users.noreply.github.com>
2025-04-21 13:10:59 -0500

    Update handle_relay_invs.go

--- app/protocol/flows/v5/blockrelay/handle_relay_invs.go
+++ app/protocol/flows/v5/blockrelay/handle_relay_invs.go
@@ -343,10 +343,7 @@ func (flow *handleRelayInvsFlow) processBlock(block *externalapi.DomainBlock) ([
 	err := flow.Domain().Consensus().ValidateAndInsertBlock(block, true)
 	if err != nil {
 		if !errors.As(err, &ruleerrors.RuleError{}) {
-			err = flow.processOrphan(block)
-			if err == nil {
-				return nil, nil
-			}
+
 			return nil, errors.Wrapf(err, "failed to process block %s", blockHash)
 		}
 		missingParentsError := &ruleerrors.ErrMissingParents{}
