8a4e252ef0aa02d2d6550a250822dbd8c9d4640c
kobradag <168386458+kobradag@users.noreply.github.com>
2025-05-01 12:50:16 -0500

    Update transactions_pool.go

--- domain/miningmanager/mempool/transactions_pool.go
+++ domain/miningmanager/mempool/transactions_pool.go
@@ -83,8 +83,7 @@ func (tp *transactionsPool) removeTransaction(transaction *model.MempoolTransact
 	err := tp.transactionsOrderedByFeeRate.Remove(transaction)
 	if err != nil {
 		if errors.Is(err, model.ErrTransactionNotFound) {
-			log.Errorf("Transaction %s not found in tp.transactionsOrderedByFeeRate. This should never happen but sometime does",
-				transaction.TransactionID())
+			// Transaction not found - continue without logging
 		} else {
 			return err
 		}
